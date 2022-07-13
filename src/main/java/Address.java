37c92df16f2e290fdb707b85620c2ee7a1e29534
오지훈 <huni1006@lezhin.com>
2022-07-14 03:29:37 +0900

    Impl Address

---
+++ src/main/java/Address.java
@@ -0,0 +1,75 @@
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import lombok.NonNull;
+import org.apache.commons.lang3.Range;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
+
+/**
+ * 역할.
+ * message 의 목적지에 대한 값( ip string 을[원시값]) 객체로 다루기 위해 사용한다.
+ *
+ * vo 란.
+ * 원시 값은 해석하는 방법에 따라 다양하게 해석되어질 수 있다.
+ * 192.168.0.1 이 ip address 가 아니라 passwd 로 사용될 수 있다.
+ * 다르게 해석 됨에따라 전혀 다른 의도의 로직으로 사용될 가능성이 있다.
+ * 이를 방지하기 위해, 원시값을 객체로 감싸고 하나의 의미로 사용하기 위해 vo 객체를 구성한다.
+ *
+ * vo 간의 연산을 위해 equal, hash code 구현.
+ */
+public class Address {
+    private static final Range<Integer> DIGIT_RANGE = Range.between(0, 255);
+    private static final int PART_SIZE = 4;
+    private static final String DELIMITER = "\\.";
+    private final String value;
+    public Address(@NonNull String value) {
+        if(StringUtils.isEmpty(value)){
+            throw new RuntimeException("value 가 empty 입니다.");
+        }
+        if(StringUtils.isBlank(value)){
+            throw new RuntimeException("value 가 blanck 입니다.");
+        }
+
+        this.value = validate(value);
+    }
+
+    private static String validate(String value) {
+        String[] splitAddress = value.split(DELIMITER);
+
+        if(notAddressPartSize(splitAddress.length)){
+            throw new RuntimeException("not ipV4 address part size.");
+        }
+        if(notNumbericAddresses(splitAddress)){
+            throw new RuntimeException("not numberic address.");
+        }
+        if(notSuitableRange(Stream.of(splitAddress).map(Integer::parseInt).collect(Collectors.toUnmodifiableList()))){
+            throw new RuntimeException("not suitable range.");
+        }
+        return value;
+    }
+
+    private static boolean notSuitableRange(List<Integer> splitedAddress) {
+        return splitedAddress.stream().anyMatch(it -> !DIGIT_RANGE.contains(it));
+    }
+    private static boolean notNumbericAddresses(String[] splitedAddress) {
+        return Stream.of(splitedAddress).anyMatch( it -> !NumberUtils.isParsable(it));
+    }
+    private static boolean notAddressPartSize(int splitAddressSize) {
+        return PART_SIZE != splitAddressSize;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(value, address.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
---
+++ src/test/java/AddressTest.java
@@ -0,0 +1,165 @@
+import java.util.Objects;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+/**
+ * 역할일치?
+ * string 포멧의 ip address 를 vo 로 다루기 위해 address 객체를 생성하였다.
+ * address 객체를 값으로써 다루기 위해 equal, hashcode 을 구현하였다.
+ *
+ * test suit 복잡도?
+ * vo 를 생성을 위한 string 값 하나가 필요하다. 그렇기 때문에 복잡도는 높지않다.
+ * 하지만, vo 객체의 value 의 조건이 까다롭기 때문에 다양한 테스트가 필요하다.
+ *
+ * 테스트 코드의 이상한점?
+ * 없는것 같습니다.
+ */
+class AddressTest {
+
+    @DisplayName("ipv4 포멧을 가진 값을 받으면 객체가 생성됩니디.")
+    @ParameterizedTest
+    @ValueSource(strings = {"001.001.001.001","192.168.0.1", "225.1.1.1", "123.123.123.123", "233.0.1.0", "255.255.255.255"})
+    void test2(String address){
+        //given
+        //when
+        Throwable actual = Assertions.catchThrowable(()-> new Address(address));
+
+        //then
+        Assertions.assertThat(actual)
+            .isNull();
+    }
+
+    @DisplayName("address 값이 empty, blank, null 이면 exception 이 발생합니다.")
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = " ")
+    void test1(String address){
+        //given
+        //when
+        Throwable actual = Assertions.catchThrowable(() -> new Address(address));
+
+        //then
+        Assertions.assertThat(actual)
+            .isNotNull();
+    }
+
+    @DisplayName("address 값이 ipv4 포멧의 사이즈로 분할되지 않으면 exception 이 발생합니다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "1.2", "1.2.3", "1.2.3.4.5", "1.2.3.4.5.6", "1.2.3.4.5.6.7"})
+    void test3(String address) {
+        //given
+        //when
+        Throwable actual = Assertions.catchThrowable(()-> new Address(address));
+
+        //then
+        Assertions.assertThat(actual)
+            .isNotNull();
+    }
+
+    @DisplayName("address 값이 숫자가 아니면 exception 이 발생합니다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"aaa.aaa.aaa.aaa", "aaa.aaa.bbb.ccc", "aaa.bbb.ccc.ddd", "123.123.123.abc", "123.123.123.!!!"})
+    void test4(String address){
+        //given
+        //when
+        Throwable actual = Assertions.catchThrowable(()-> new Address(address));
+
+        //then
+        Assertions.assertThat(actual)
+            .isNotNull();
+    }
+
+    @DisplayName("address 값이 ipv4 프로토콜의 범위에 존재하지 않으면 exception 이 발생합니다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"001.001.001.256", "001.001.256.256", "001.256.256.256", "256.256.256.256", "1000.1000.1000.1000"})
+    void test5(String address) {
+        //given
+        //when
+        Throwable actual = Assertions.catchThrowable(()-> new Address(address));
+
+        //then
+        Assertions.assertThat(actual)
+            .isNotNull();
+    }
+
+    @DisplayName("값이 동일한 객체이면 eqaul 비교시 true 를 반환합니다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"001.001.001.001", "002.002.002.002", "123.123.123.123", "225.225.225.225", "1.2.3.4"})
+    void test6(String address){
+        //given
+        Address address1 = new Address(address);
+        Address address2 = new Address(address);
+
+        //when
+        boolean actual = Objects.equals(address1, address2);
+
+        //then
+        Assertions.assertThat(actual)
+            .isTrue();
+    }
+
+    @DisplayName("값이 동일한 객체가 아니면 equal 비교시 false 를 반환합니다.")
+    @ParameterizedTest
+    @CsvSource(value= {
+        "001.001.001.001, 001.001.001.002",
+        "002.002.002.002, 003.003.003.003",
+        "123.123.123.123, 123.123.123.124",
+        "225.225.225.224, 225.225.225.225",
+    })
+    void test7(String sAddress1, String sAddress2){
+        //given
+        Address address1 = new Address(sAddress1);
+        Address address2 = new Address(sAddress2);
+
+        //when
+        boolean actual = Objects.equals(address1, address2);
+
+        //then
+        Assertions.assertThat(actual)
+            .isFalse();
+    }
+
+    @DisplayName("값이 동일한 객체이면 같은 hash code 를 생성합니다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"001.001.001.001", "002.002.002.002", "123.123.123.123", "225.225.225.225", "1.2.3.4"})
+    void test8(String sAddress){
+        //given
+        int hashCode1 = new Address(sAddress).hashCode();
+        int hashCode2 = new Address(sAddress).hashCode();
+
+        System.out.println(hashCode1);
+        System.out.println(hashCode2);
+
+        //when
+        boolean actual = Objects.equals(hashCode1, hashCode2);
+
+        //then
+        Assertions.assertThat(actual)
+            .isTrue();
+    }
+
+    @DisplayName("값이 동일한 객체가 아니면 다른 hash code 를 생성합니다.")
+    @ParameterizedTest
+    @CsvSource(value= {
+        "001.001.001.001, 001.001.001.002",
+        "002.002.002.002, 003.003.003.003",
+        "123.123.123.123, 123.123.123.124",
+        "225.225.225.224, 225.225.225.225"
+    })
+    void test9(String sAddress1, String sAddress2){
+        //given
+        int hashCode1 = new Address(sAddress1).hashCode();
+        int hashCode2 = new Address(sAddress2).hashCode();
+
+        //when
+        boolean actual = Objects.equals(hashCode1, hashCode2);
+
+        //then
+        Assertions.assertThat(actual)
+            .isFalse();
+    }
+}
